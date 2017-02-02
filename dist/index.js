19aef635d61e2bb7ea30f0a34b0b0e81c571e458
Lorenzofox3 <laurent34azerty@gmail.com>
2017-02-02 11:53:17 -0500

    fix use the normalized clauses

--- dist/index.js
+++ dist/index.js
@@ -60,6 +60,9 @@ const operators = {
   is(value){
     return (input) => Object.is(value, input);
   },
+  isNot(value){
+    return (input) => !Object.is(value, input);
+  },
   lt(value){
     return (input) => input < value;
   },
@@ -74,23 +77,39 @@ const operators = {
   },
   equals(value){
     return (input) => value == input;
+  },
+  notEquals(value){
+    return (input) => value != input;
   }
 };
 
 const every = fns => (...args) => fns.every(fn => fn(...args));
 
-function predicate ({value = '', operator = 'includes', type = ''}) {
+function predicate ({value = '', operator = 'includes', type = 'string'}) {
   const typeIt = typeExpression(type);
   const operateOnTyped = compose(typeIt, operators[operator]);
   const predicateFunc = operateOnTyped(value);
   return compose(typeIt, predicateFunc);
 }
 
+//avoid useless filter lookup (improve perf)
+function normalizeClauses (conf) {
+  const output = {};
+  const validPath = Object.keys(conf).filter(path => Array.isArray(conf[path]));
+  validPath.forEach(path => {
+    const validClauses = conf[path].filter(c => c.value !== '');
+    if (validClauses.length) {
+      output[path] = validClauses;
+    }
+  });
+  return output;
+}
+
 function filter (filter) {
-  const validPathList = Object.keys(filter).filter(path => Array.isArray(filter[path]) && filter[path].length);
-  const funcList = validPathList.map(path => {
+  const normalizedClauses = normalizeClauses(filter);
+  const funcList = Object.keys(normalizedClauses).map(path => {
     const getter = pointer(path).get;
-    const clauses = filter[path].map(predicate);
+    const clauses = normalizedClauses[path].map(predicate);
     return compose(getter, every(clauses));
   });
   const filterPredicate = every(funcList);
